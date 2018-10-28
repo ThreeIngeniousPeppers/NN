7776b3a2ef125c894bcc6c45506a3579f593e1db
danil <shalagin.danil@mail.ru>
2018-10-28 05:03:53 +0300

    Add graphics

--- View/global.js
+++ View/global.js
@@ -1,3 +1,8 @@
+var myResult;
+var actualPoints,
+    predictedPoints,
+    times;
+
 $(document).ready(function() {
     $.ajax({
         type: "GET",
@@ -6,9 +11,13 @@ $(document).ready(function() {
         success: function(data) {
 
             var parsedData = processData(data);
-
+            myResult = parsedData;
             console.log(parsedData);
+            times = Array.from(myResult, x=>x.time);
+            actualPoints = Array.from(myResult, x=>x.actual);
+            predictedPoints = Array.from(myResult, x=>x.predicted);
 
+            renderChart();
         }
      });
 });
@@ -28,4 +37,85 @@ function processData(allText) {
        resultArray.push(rowObject);
     }
     return resultArray;
+}
+
+function renderChart() {
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#292b2c';
+
+    // Area Chart Example
+    var ctx = document.getElementById("myAreaChart");
+
+    var options = {
+        scales: {
+                  xAxes: [{
+                    time: {
+                      unit: 'date'
+                    },
+                    gridLines: {
+                      display: false
+                    },
+                    ticks: {
+                      maxTicksLimit: 7
+                    }
+                  }],
+                  yAxes: [{
+                    ticks: {
+                      min: 6468.2996,
+                      max: 6468.30075,
+                      maxTicksLimit: 0.0000000001
+                    },
+                    gridLines: {
+                      color: "rgba(0, 0, 0, .125)",
+                    }
+                  }]
+        },
+        legend: {
+                  display: false
+                }
+    };
+
+    var dataInLine = {
+         labels: times,
+         datasets: [{
+           label: "Actual",
+           lineTension: 0.3,
+           backgroundColor: "rgba(2,117,216,0.2)",
+           borderColor: "rgba(2,117,216,1)",
+           pointRadius: 5,
+           pointBackgroundColor: "rgba(2,117,216,1)",
+           pointBorderColor: "rgba(255,255,255,0.8)",
+           pointHoverRadius: 5,
+           pointHoverBackgroundColor: "rgba(2,117,216,1)",
+           pointHitRadius: 50,
+           pointBorderWidth: 2,
+           data: actualPoints,
+         },
+         {
+            label: "Predicted",
+            lineTension: 0.3,
+            backgroundColor: "rgba(2,117,216,0.2)",
+            borderColor: "rgba(216,24,2,1)",
+            pointRadius: 5,
+            pointBackgroundColor: "rgba(216,24,2,1)",
+            pointBorderColor: "rgba(216,24,2,0.8)",
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(2,117,216,1)",
+            pointHitRadius: 50,
+            pointBorderWidth: 2,
+            data: predictedPoints,
+
+                  }
+         ],
+       };
+
+
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: dataInLine,
+      options: options,
+    });
+
+
 }
